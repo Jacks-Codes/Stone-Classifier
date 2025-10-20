ba77d730f30c07bb314e0c9f254c1b7bee1b10d9
Jacks-Codes <jacksoncopeland79@gmail.com>
2025-10-20 10:37:40 -0700

    Initial TensorFlow set up and gitignore

---
+++ Test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+print("This is a test, Version: ", tf.__version__)
+print("GPU Available: ", tf.config.list_physical_devices('GPU'))
+
+mnist = tf.keras.datasets.mnist
+(x_train,y_train),(x_test, y_test) = mnist.load_data()
+print("Tensor Flow is working")
+
